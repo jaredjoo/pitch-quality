f0c06c3340b55b52c1570cd54cb547f3dbe93012
Jared Joo <jared7e1@gmail.com>
2023-04-30 14:57:58 -0400

    added sorting and searching to the table

--- src/components/Pages/Results.js
+++ src/components/Pages/Results.js
@@ -13,6 +13,7 @@ import {
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
+// set the dimensions of the table
 const useStyles = makeStyles({
   table: {
     minWidth: 500,
@@ -22,22 +23,50 @@ const useStyles = makeStyles({
   },
 });
 
+// sort the data according to the fields
+const sortData = (data, orderBy, order) => {
+  const sortedData = [...data];
 
+  // sort in ascending order
+  sortedData.sort((a, b) => {
+    let result;
+    if (order === "asc") {
+      if (a[orderBy] < b[orderBy]) {
+        result = -1;
+      } else {
+        result = 1;
+      }
+    } else {
+      if (a[orderBy] > b[orderBy]) {
+        result = -1;
+      } else {
+        result = 1;
+      }
+    }
+    return result;
+  });
+  return sortedData;
+};
 
 
 export const Results = () => {
   const classes = useStyles();
   const [playerData, setPlayerData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [order, setOrder] = useState("asc");
+  const [orderBy, setOrderBy] = useState("player_name");
 
-
+  // continuoslyu update the table with the csv
   useEffect(() => {
     const fetchData = async () => {
+      // gett the data from csv and conver to array
       const response = await fetch(bestPitches);
       const data = await response.text();
       let parsedData = data.split("\n").map((row) => row.split(","));
       while (parsedData.length > 0 && parsedData[parsedData.length - 1].every((cell) => !cell)) {
         parsedData.pop();
       }
+      // extract headers and set them as keys
       const headers = parsedData.shift();
       const players = parsedData.map((row) =>
         headers.reduce((obj, header, index) => {
@@ -45,19 +74,34 @@ export const Results = () => {
           return obj;
         }, {})
       );
+
+      // update the state upon chagne
       setPlayerData(players);
     };
     fetchData();
   }, []);
 
+  // sort according to key
+  const createSortHandler = (property) => (event) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
 
+  // filter according to player name through the search box
+  const filteredData = playerData.filter((player) =>
+    player.player_name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-
-  
+  const sortedData = sortData(filteredData, orderBy, order);
 
   return (
     <div>
-
+      <TextField
+        label="Search Player"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
       <TableContainer component={Paper} className={classes.tableContainer}>
         <Table className={classes.table} stickyHeader>
           <TableHead>
@@ -66,6 +110,7 @@ export const Results = () => {
                 <TableSortLabel
                   active={orderBy === "player_name"}
                   direction={order}
+                  onClick={createSortHandler("player_name")}
                 >
                   Player Name
                 </TableSortLabel>
@@ -74,6 +119,7 @@ export const Results = () => {
                 <TableSortLabel
                   active={orderBy === "pitch_type"}
                   direction={order}
+                  onClick={createSortHandler("pitch_type")}
                 >
                   Pitch Type
                 </TableSortLabel>
@@ -82,6 +128,7 @@ export const Results = () => {
                 <TableSortLabel
                   active={orderBy === "stuffGrade"}
                   direction={order}
+                  onClick={createSortHandler("stuffGrade")}
                 >
                   Stuff Grade
                 </TableSortLabel>
@@ -90,13 +137,13 @@ export const Results = () => {
                 <TableSortLabel
                   active={orderBy === "nPitches"}
                   direction={order}
+                  onClick={createSortHandler("nPitches")}
                 >
-                  nPitches
+                  Number of Pitches
                 </TableSortLabel>
               </TableCell>
             </TableRow>
           </TableHead>
-
           <TableBody>
             {sortedData.map((row, index) => (
               <TableRow key={index}>
